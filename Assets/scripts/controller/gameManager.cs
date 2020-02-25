bf5e62121546118ced5da52e5461dc37b28e5fc7
simonsalamon <salamon.1378@gmail.com>
2020-02-25 17:39:59 +0330

    change game

--- Assets/scripts/controller/gameManager.cs
+++ Assets/scripts/controller/gameManager.cs
@@ -9,8 +9,6 @@ public enum gameState
 
 public class gameManager : singelton<gameManager>
 {
-    [SerializeField] private GameObject expo;
-    [SerializeField] private GameObject Enemy;
     [SerializeField] private Transform player;
     [SerializeField] private float offcet; 
     [SerializeField] private List<Transform> pos;
@@ -32,22 +30,6 @@ public class gameManager : singelton<gameManager>
         // } 
     // }
 
-    public void initEnemy(GameObject EnemyCar){
-        Transform newPos = pos[Random.Range(0,4)];
-        GameObject enemy = Instantiate(EnemyCar ,newPos.position, Quaternion.identity) as GameObject;
-        PoliceCar car =  enemy.GetComponent<PoliceCar>();
-        car.GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-1.9f,0); 
-        car.PlayerCar = player;
-    }
-
-    public void explorecar(Vector3 position){
-        // here we explore cars when collide
-        GameObject go = Instantiate(expo , position , Quaternion.identity) as GameObject;
-        go.transform.rotation = expo.transform.rotation;
-        ParticleSystem particle = go.GetComponent<ParticleSystem>();
-        particle.enableEmission = true;
-        Destroy(go,10);
-    }
     public void loseGame(){
         state = gameState.lose;
     }
